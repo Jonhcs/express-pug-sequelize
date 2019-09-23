996699767b7a7dbe4874f33e33a25217287f53df
Jhonatan Carvalho <srjhonatancarvalho@gmail.com>
2019-09-23 19:22:28 -0300

    error sequelize

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
+const Sequelize = require('sequelize')
 
 const app = express()
 
-
+//const sequelize = new Sequelize('mysql://root:hello123@localhost:3000/sequelize')
+const sequelize = new Sequelize('sequelize', 'root', 'info@1234', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: Sequelize.Op
+})
+sequelize
+    .authenticate()
+    .then(() => console.log('OK!'))
+    .catch(() => console.log('erro!'))
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false}))
